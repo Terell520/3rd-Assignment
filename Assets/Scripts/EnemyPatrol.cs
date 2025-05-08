a1606a3cb465bc4a96da733f3522b5110b68120b
Terell520 <124600032+Terell520@users.noreply.github.com>
2025-05-08 18:35:35 -0400

    Update 2.0
    Enemy Patrol developed

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -2,14 +2,30 @@ using UnityEngine;
 
 public class EnemyPatrol : MonoBehaviour
 {
-    public UnityEngine.AI.NavMeshAgent agent;
+    public Transform[] patrolPoints;
+    public int targetPoint;
+    public float speed = 10f;
 
-    public Transform Anna;
+    private void Start()
+    {
+        targetPoint = 0;
+    }
 
-    public LayerMask whosanna, ground;
+    private void Update()
+    { 
+        if (Vector3.Distance(transform.position, patrolPoints[targetPoint].position)<= 0.1)
+        {
+            increaseTargetInt();
+        }
+        transform.position = Vector3.Lerp(transform.position, patrolPoints[targetPoint].position, speed * Time.deltaTime);
+    }
 
-    //Patrolling
-    public Vector3 walkPoint;
-    bool walkPointSet;
-    public bool walkPointRange;
+    void increaseTargetInt()
+    {
+        targetPoint++;
+        if (targetPoint >= patrolPoints.Length)
+        {
+            targetPoint = 0;
+        }
+    }
 }
